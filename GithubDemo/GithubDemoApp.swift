7a48ee4df10206c20927f842c4410eaa7c8a4c26
Nathan Chuluda <nchuluda23@msu.idserve.net>
2025-01-21 12:04:12 -0500

    Initial Commit

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by Nathan on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
